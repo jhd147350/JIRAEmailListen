f89c7ae7ff874b2777675fc4b2747679f36005af
jaden <18636621167@163.com>
2016-12-25 17:52:34 +0800

    场景添加邮件监听功能 修改邮件监听方式

--- hydra/src/test/java/com/test/Client.java
+++ hydra/src/test/java/com/test/Client.java
@@ -1,20 +1,49 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hydra.importmailmessage.imp.ImportMessageToJIRA;
 import hydra.importmessage.inter.ImportMessage;
+import hydra.mailbox.abs.AbstractMailboxListen;
 import hydra.mailbox.imp.QQEmailboxListen;
 import hydra.tool.MailboxType;
+import hydra.vo.JIRAAccount;
 import hydra.vo.MailboxAccount;
 import hydra.vo.inter.Account;
-import hydra.vo.inter.Message;
-import hydra.vo.JIRAAccount;
 public class Client {
 	public static void main(String args[]){
-		Account emailaccount=new MailboxAccount(MailboxType.WY163, null, "段雪超", "ddd");
+		/*Account emailaccount=new MailboxAccount(MailboxType.WY163, null, "段雪超", "ddd");
 		Account jiraaccount=new JIRAAccount("张三", "ddd");
 		QQEmailboxListen emailboxListen=new QQEmailboxListen(null);
 		Message emailmessage=emailboxListen.getEmail((MailboxAccount)emailaccount);
 		ImportMessage mailtojira=new ImportMessageToJIRA();
-		mailtojira.execute(jiraaccount, emailmessage);
+		mailtojira.execute(jiraaccount, emailmessage);*/
+		test1();
+	}
+	public static void test1(){
+		List<MailboxAccount> accounts=new ArrayList<MailboxAccount>();
+		accounts.add(new MailboxAccount(MailboxType.QQ, null, "张三", "dd"));
+		accounts.add(new MailboxAccount(MailboxType.QQ, null, "李四", "dd"));
+		accounts.add(new MailboxAccount(MailboxType.QQ, null, "王五", "dd"));
+		AbstractMailboxListen listen =new QQEmailboxListen(null);
+		Account jiraaccount=new JIRAAccount("张三", "ddd");
+		ImportMessage mailtojira=new ImportMessageToJIRA();
+		listen.getEmail(accounts);
+		synchronized(listen){
+			while(true){
+				try {
+					listen.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(listen.mailMessage!=null){
+					mailtojira.execute(jiraaccount, listen.mailMessage);
+					listen.notify();
+				}
+				
+			}
+		}
 	}
 }
